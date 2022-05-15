428028c1e87674dcd2de21533b990ba58fd5d974
Francisco <zyx.assis@gmail.com>
2022-05-15 14:46:11 -0300

    Feature coreapp done. File import Health Unity

---
+++ coreapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from coreapp import models
+
+
+admin.site.register(models.HealthUnity)
+admin.site.register(models.HealthUnityFile)
---
+++ coreapp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from coreapp import views
+
+app_name = 'coreapp'
+
+urlpatterns = [
+    path('ubs/import/', 
+        views.health_unities_import,
+         name='health_unities_import'),
+    path('ubs/file/list/', 
+        views.health_unity_file_list, 
+        name='health_unity_file_list'),
+    path('ubs/file/detail/<int:health_unity_file_pk>/',
+         views.health_unity_file_detail, 
+        name='health_unity_file_detail'),
+    path('ubs/file/list/<int:health_unity_file_pk>/',
+         views.health_unity_list, 
+        name='health_unity_list')
+]
